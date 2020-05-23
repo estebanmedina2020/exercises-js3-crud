ec049af733d976de33f97ea8f2c29c27637d8cab
Esteban <emedina@creatonico.com>
2020-05-23 17:06:40 +0200

    updated code

--- js/main.js
+++ js/main.js
@@ -8,10 +8,14 @@ console.log(await getFunction);
 async function main() {
   const result = await get();
   console.log(result);
-  updateTable(result);
+  updateTable(result.items);
 }
 async function get() {
-  return await fetch("../json/list.json").then((response) => response.json());
+  return await fetch("https://api.typeform.com/forms", {
+    headers: {
+      Authorization: "Bearer B9F2AmcV1BEvEMgSrBMdLhgjnxJszME8yKKHELB9wdVe",
+    },
+  }).then((response) => response.json());
 }
 main();
 
@@ -20,7 +24,22 @@ const table = document.getElementById("table");
 function updateTable(results) {
   results.forEach((result) => {
     const row = document.createElement("tr");
-    row.innerText = result.title;
+
+    const title = document.createElement("td");
+    title.innerText = result.title;
+    row.appendChild(title);
+
+    const href = document.createElement("td");
+    href.innerText = result.href;
+    row.appendChild(href);
+
+    const remove = document.createElement("td");
+    const removeLink = document.createElement("a");
+    removeLink.innerText = "Remove";
+    removeLink.setAttribute("href", "#");
+    remove.appendChild(removeLink);
+    row.appendChild(remove);
+
     table.appendChild(row);
   });
 }
